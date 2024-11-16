89bd335f6054e75a5003cb700a4231dfc544683f
Volodymyr Tkach <teamco@gmail.com>
2024-11-16 21:50:26 +0200

    fix routing

--- config/routes/main.routes.js
+++ config/routes/main.routes.js
@@ -1,11 +1,9 @@
 const { ERRORS } = require('./error.routes');
-const { ADMIN_ROUTES } = require('./admin.routes');
 
 const wrappers = ['@/wrappers/auth.consumer'];
 
 const MAIN_ROUTES = (mainPath = '') => {
   const mainErrors = ERRORS(mainPath);
-  const adminRoutes = ADMIN_ROUTES();
 
   return {
     exact: false,
@@ -15,9 +13,8 @@ const MAIN_ROUTES = (mainPath = '') => {
       {
         exact: true,
         path: `${mainPath}/`,
-        redirect: '/ex/cards'
+        redirect: '/flip-cards'
       },
-      adminRoutes,
       ...mainErrors
     ]
   };
